b843731d19de2e4dd9f80735d74eb004eedb1fa3
s432757 <03041999kirill@gmail.com>
2018-10-04 13:04:57 +0200

    kek

--- src/main/java/Pracownia/Introduction/Main.java
+++ src/main/java/Pracownia/Introduction/Main.java
@@ -1,8 +1,13 @@
 package Pracownia.Introduction;
 
+import org.apache.log4j.Logger;
+
 public class Main {
 
+static Logger log = Logger.getLogger("abc");
     public static void main(String [ ] args) {
-        System.out.println("Hello World");
+        log.info("massege");
+        System.out.println("hello world");
     }
+
 }
